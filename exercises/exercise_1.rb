b0c444804b4da85524ecf8336554b1a97d6c6865
Donnell Harris <donnell.harris@koodomobile.com>
2023-01-24 12:39:34 -0800

    completed exercise 1 & 2

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -4,3 +4,14 @@ puts "Exercise 1"
 puts "----------"
 
 # Your code goes below here ...
+class Store # < ApplicationRecord::Base
+end
+
+burnabyStore = Store.create(name: "Burnaby", annual_revenue: 300000, mens_apparel: true, womens_apparel: true)
+
+richmondStore = Store.create(name: "Richmond", annual_revenue: 1260000, mens_apparel: false, womens_apparel: true)
+
+gastownStore = Store.create(name: "Gastown", annual_revenue: 190000, mens_apparel: true, womens_apparel: false)
+
+puts Store.count
+
--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -1,7 +1,16 @@
 require_relative '../setup'
 require_relative './exercise_1'
+require 'pp'
 
 puts "Exercise 2"
 puts "----------"
 
 # Your code goes here ...
+@store1 = Store.find_by(id: 1)
+
+pp Store.all
+
+@store2 = Store.find_by(id: 2)
+
+@store1.update(name: 'Something')
+
